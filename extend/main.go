b53adabd9c0f56f58ab6fb63623128cf719bb406
taeksoolee <leets1490@gmail.com>
2026-06-07 02:24:42 +0900

    fix: PocketBase v0.22 버전에 맞게 이벤트 훅 문법 수정

--- extend/main.go
+++ extend/main.go
@@ -10,12 +10,10 @@ import (
 func main() {
 	app := pocketbase.New()
 
-	// 예시: 레코드 생성 후 커스텀 Go 로직 실행
-	// JS 훅으로 처리하기 어려운 복잡한 비즈니스 로직이나
-	// 외부 Go 패키지가 필요한 경우 이곳에 작성한다.
-	app.OnRecordAfterCreateSuccess().BindFunc(func(e *core.RecordEvent) error {
+	app.OnRecordAfterCreate().Add(func(e *core.RecordCreateEvent) error {
 		log.Printf("[hook] record created: %s / %s", e.Record.Collection().Name, e.Record.Id)
-		return e.Next()
+		
+		return nil
 	})
 
 	if err := app.Start(); err != nil {
