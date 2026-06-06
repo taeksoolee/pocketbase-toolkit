06cda069f3845b5b8a5c6828710f0b6418a6040e
taeksoolee <leets1490@gmail.com>
2026-06-07 02:16:18 +0900

    fix: main.go 컴파일 에러 해결 및 PocketBase v0.23 대응

--- extend/main.go
+++ extend/main.go
@@ -10,12 +10,14 @@ import (
 func main() {
 	app := pocketbase.New()
 
-	// 예시: 레코드 생성 후 커스텀 Go 로직 실행
-	// JS 훅으로 처리하기 어려운 복잡한 비즈니스 로직이나
-	// 외부 Go 패키지가 필요한 경우 이곳에 작성한다.
-	app.OnRecordAfterCreateSuccess().BindFunc(func(e *core.RecordEvent) error {
-		log.Printf("[hook] record created: %s / %s", e.Record.Collection().Name, e.Record.Id)
-		return e.Next()
+	app.OnRecordCreateExecute().BindFunc(func(e *core.RecordCreateEvent) error {
+		if err := e.Next(); err != nil {
+			return err // 생성 중 에러가 나면 즉시 중단 및 롤백
+		}
+
+		log.Printf("[hook] record created: %s / %s", e.Collection.Name, e.Record.Id)
+
+		return nil
 	})
 
 	if err := app.Start(); err != nil {
